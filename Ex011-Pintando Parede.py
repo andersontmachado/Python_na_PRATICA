8cbe52e40ec3f2cdf00ac3ca2eb1b6e0d43de300
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-05-05 10:24:52 -0300

    Encontrando quantos litros de tinta para o m²

---
+++ Ex011-Pintando Parede.py
@@ -0,0 +1,6 @@
+l=float(input('Largura da parede: '))
+a=float(input('Altura da parede: '))
+m=l*a
+print('Sua parede tem dimensão de {} x {} e sua àrea é de {}m²'.format(l,a,m))
+t=m/2
+print('Para pintar a parede você precisará de {}l de tinta'.format(t))
