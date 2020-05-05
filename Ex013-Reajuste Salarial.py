01dd5943f8319f398db10d241cfac17077095cfa
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-05-05 11:21:47 -0300

    Exercício Python 013: Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.

--- Ex013-Reajuste Salarial.py
+++ Ex013-Reajuste Salarial.py
@@ -1,3 +1,4 @@
 salario=float(input('Qual é o salário do funcionario R$'))
 novo=salario+(salario*15/100)
 print('Um funcionário que ganhava {:.2f} com aumento de 15%,passa a receber R${:.2f}'.format(salario,novo))
+#
