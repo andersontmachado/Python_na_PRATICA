48ce131b077d9d1e8a972cd17c43abca20c4856f
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-05-05 19:00:08 -0300

    Exercício Python 016: Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.

---
+++ Ex016-Quebrando um número.py
@@ -0,0 +1,9 @@
+'''from math import trunc
+n=float(input('Digite um valor: '))
+print('O valor digititado foi {} e sua porção inteira é {}'.format(n,trunc(n)))'''
+
+
+#nem smepre precisamos importar o math,também existe a maneira de colocar o numero inteiro como int
+
+n=float(input('Digite um valor: '))
+print('O valor digitado foi {} e sua porção inteira é {}'.format(n , int(n)))
