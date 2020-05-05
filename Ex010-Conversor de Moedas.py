80db2a5dd15a289db85d92e6c281c2f8b246c912
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-05-05 10:19:54 -0300

    Tabuada com .format

---
+++ Ex010-Conversor de Moedas.py
@@ -0,0 +1,7 @@
+d=float(input('Quanto de dinheiro você tem na carteira:R$ '))
+dolar=d/5.52
+e=d/6.00
+print('Com {:.2f} você pode comprar US${:.2f}.'.format(d,dolar))
+print('Com {:.2f} você pode comprar EURO {:.2f}.'.format(d,e))
+#Estamos passando por um vírus invisível,por isso está tão alto.Xô#Corona
+
