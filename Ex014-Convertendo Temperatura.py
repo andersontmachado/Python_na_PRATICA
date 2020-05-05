d85a57d37deb6f903b54b3a74f514914ee33c57b
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-05-05 11:22:55 -0300

    Exercício Python 014: Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.

--- Ex014-Convertendo Temperatura.py
+++ Ex014-Convertendo Temperatura.py
@@ -4,4 +4,4 @@ f=((9*c)/5)+32
 
 print('A temperatura de {}ºC corresponde a {}°F'.format(c,f))
 
-#esse exercicio pode fazer sem parenteses na formula,pois tem a precendia certa.
+#esse exercicio pode fazer sem parenteses na formula,pois tem a precendia certa..
