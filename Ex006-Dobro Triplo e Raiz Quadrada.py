d9887f1bce3e4bd7d0fee7a66a9e72d520761454
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-05-05 10:04:03 -0300

    Cálculos do dobro,triplo e raiz quadrada

---
+++ Ex006-Dobro Triplo e Raiz Quadrada.py
@@ -0,0 +1,7 @@
+n=int(input('Digite um número: '))
+d=n*2
+t=n*3
+r=n**(1/2)
+print('O dobro do número {} é {}.'.format(n,d))
+print('O triplo do número {} é {}.'.format(n,t))
+print('A raiz quadrada do número {} é {:.0f}.'.format(n,r))
