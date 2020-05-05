f2905501f2fd516c92ce0d3f5670d57c7e034fd2
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-05-05 10:37:27 -0300

    Convertendo Celsius para Fahrneit

---
+++ Ex014-Convertendo Temperatura.py
@@ -0,0 +1,7 @@
+c=float(input('Qual a temperatura em ºC:'))
+f=((9*c)/5)+32
+
+
+print('A temperatura de {}ºC corresponde a {}°F'.format(c,f))
+
+#esse exercicio pode fazer sem parenteses na formula,pois tem a precendia certa.
