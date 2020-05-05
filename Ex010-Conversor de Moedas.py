93202fb7f7818ab434b46423b4aabc443536d3b1
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-05-05 11:16:31 -0300

    Exercício Python 010: Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.

--- Ex010-Conversor de Moedas.py
+++ Ex010-Conversor de Moedas.py
@@ -3,4 +3,4 @@ dolar=d/5.52
 e=d/6.00
 print('Com {:.2f} você pode comprar US${:.2f}.'.format(d,dolar))
 print('Com {:.2f} você pode comprar EURO {:.2f}.'.format(d,e))
-#Estamos passando por um vírus invisível,por isso está tão alto.Xô#Corona
+#Estamos passando por um vírus invisível,por isso está tão alto.Xô#Coronaaa
