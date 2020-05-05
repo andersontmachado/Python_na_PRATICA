d2034df1f9ca706dfa1efefa4e73c33699348c65
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-05-05 19:29:40 -0300

    Exercício Python 021: Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.

---
+++ Ex021-Tocando um mp3.py
@@ -0,0 +1,21 @@
+import pygame
+pygame.mixer.init()
+pygame.mixer.music.load('thiaguinho.mp3')
+pygame.mixer.music.load('thiago.mp3')
+pygame.mixer.music.play()
+input()
+pygame.event.wait()
+### coloquei INPUT() pelo menos funciono...então colocamos esse código debaixo.
+
+''''# Inicializando o mixer PyGame
+pygame.mixer.init()
+
+# Iniciando o Pygame
+pygame.init()
+pygame.mixer.music.load('thiaguinho.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
+
+#dificil pra caramba esse.'''
+
+
