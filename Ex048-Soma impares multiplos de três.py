5f19b426c88df2e36f7575cbc8749bb2c4da252a
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-05-05 21:55:06 -0300

    xercício Python 048: Faça um programa que calcule a soma entre todos os números que são múltiplos de três e que se encontram no intervalo de 1 até 500.

---
+++ Ex048-Soma impares multiplos de três.py
@@ -0,0 +1,21 @@
+soma=0
+cont=0
+for n in range (1,501, 2):
+  if n % 3 == 0:
+      soma+= n
+      cont+= 1
+print('A SOMA DE TODOS OS {} VALORES SOLICITADO É {}'.format(cont,soma))
+#temos dois jeitos
+#soma=soma+n
+#cont=cont+1
+
+
+
+
+
+
+
+
+
+
+
