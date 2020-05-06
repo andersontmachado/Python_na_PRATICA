bd59facc7b1569c641ea2fc4b7da144fe8efed62
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-05-05 21:15:47 -0300

    Exemplos de dentro da aula 10.

---
+++ Ex028-Aula10 .py
@@ -0,0 +1,17 @@
+nome=str(input('Qual é seu nome? '))
+if nome == 'Anderson':
+# QUANDO TEM O IF,CHAMA-SE ESTRUTURA DE CONDICIONAL SIMPLES, ISSO É UMA CONDIÇÃO
+    print('Que nome lindo você tem!')
+else:
+# ESTRUTURA CONDICIONAL COMPOSTA, ISSO SÃO CONDIÇÕES
+    print('Seu nome é tão normal.')
+print('Bom dia {}'.format(nome))
+
+
+#   IF-Verdadeiro
+#   Else- Falso
+
+#   OBS-COMANDO DO LADO ESQUERDO SEMPRE VAI ACONTECER!
+#  não podemos esquecer dos dois pontos em if e else.
+# NA OUTRA AULA ESTA EXPLICANDO A CONDIÇÃO SIMNPLIFICADA
+
---
+++ Ex028-Aula10.1.py
@@ -0,0 +1,15 @@
+n1=int(input('Digite uma nota: '))
+n2=int(input('Digite a segunda nota: '))
+m=(n1+n2)/2
+print('Sua média foi {}'.format(m))
+if m >= 6.0:
+    print('Você foi aprovado!')
+else:
+    print('Você foi reprovado!Estude mais.')
+# O python usa a tabulação,a indexação de comandos
+#Java,PHP, muitas linguas usam as chaves,o python não
+
+#CONDIÇÃO SIMPLIFICADA
+# print('PARABENS' if m>=6 else'Estude mais')##################
+#TEM ESSAS DUAS FORMAS
+
