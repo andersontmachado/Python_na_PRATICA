ba84ac8eb86b799cd5460fa72baa74fe6505bf6a
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-05-19 22:03:45 -0300

    Exercício Python 057: Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'. Caso esteja errado, peça a digitação novamente até ter um valor correto.

--- Ex057-Validação de Dados.py
+++ Ex057-Validação de Dados.py
@@ -1,5 +1,13 @@
+lgbts=''
 sexo=str(input('Informe seu sexo: [M/F ou LGBTS] ')).strip().upper()[0]
 while sexo not in 'MFLgbts':
+    if sexo in 'LGBTS':
+        lgbts=sexo
+        print('Você é {} uma bixona!'.format(lgbts))
     sexo=str(input('Dados Inválidos.Por favor,informe seu sexo:')).strip().upper()[0]
 print('Sexo \033[32m{}\033[m registrado com sucesso'.format(sexo))
+
+
+
+
 
