ef210cf2925843e4e9ca25711edb3ccd3abbe35b
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-05-22 17:00:50 -0300

    Exercício Python 059: Crie um programa que leia dois valores e mostre um menu na tela: [ 1 ] somar [ 2 ] multiplicar [ 3 ] maior [ 4 ] novos números [ 5 ] sair do programa Seu programa deverá realizar a operação solicitada em cada caso

---
+++ Ex059-Criano menu de opções.py
@@ -0,0 +1,52 @@
+n1 = int(input('Digito o primeiro valor: '))
+n2 = int(input('Digite o segundo valor: '))
+opção=0
+while opção !=5:
+    print('''[ 1 ] somar\n[ 2 ] multiplicar\n[ 3 ] maior\n[ 4 ] novos números\n[ 5 ] sair do programa''')
+    opção=int(input('>>>>>>>>Qual sua opção?'))
+    if opção == 1:
+       soma=n1+n2
+       print('A soma de {} + {} é {}'.format(n1,n2,soma))
+    elif opção == 2:
+        multi= n1*n2
+        print('A multiplicação de {} x {} é {}'.format(n1,n2,multi))
+    elif opção == 3:
+        if n1>n2:
+            maior=n1
+        else:
+            maior=n2
+        print('O maior valor entre {} e {} é {}'.format(n1,n2,maior))
+    elif opção == 4:
+        print('Informe os números novamente: ')
+        n1=int(input('Digite o primeiro valor: '))
+        n2=int(input('Digite o segundo valor: '))
+    elif opção == 5:
+        print('Finalizando...')
+    else:
+        print('Opção Inválida.Tente novamente!')
+print('-='*12)
+print('FIM DO PROGRAMA!VOLTE SEMPRE')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
