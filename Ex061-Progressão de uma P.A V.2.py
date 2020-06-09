1663572ed10861fbe3eea92bc3e5a2166e3a78bc
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-06-09 20:37:36 -0300

    Exercício Python 061: Refaça o DESAFIO 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progressão usando a estrutura while.

--- Ex061-Progressão de uma P.A V.2.py
+++ Ex061-Progressão de uma P.A V.2.py
@@ -1,1 +1,18 @@
+print('Gerador de uma PA')
+print('=-'*15)
+primeiro=int(input('Primeiro termo: '))
+razão=int(input('Razão da P.A: '))
+termo=primeiro
+#criamos uma vriavel pro termo,vai começar com o pimeiro termo
+cont=1
+#vai contar quantos termos foram, no exercicio 51 tivenmos que criar,aqui nao precisa.
+while cont <=20:#enquanto ele não chegar a 10,vai mostra o termo
+    print('{}->'.format(termo),end='')
+#ele vai mostra alguma coisa e uma setinha vai ser o meu termo
+    termo=termo+razão
+#
+    cont+=1
+print('FIM')
+
+
 
