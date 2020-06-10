ace92d963a27a2f9d43023117eaa523f92b732c2
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-06-09 22:18:57 -0300

    Exercício Python 062: Melhore o DESAFIO 061, perguntando para o usuário se ele quer mostrar mais alguns termos. O programa encerrará quando ele disser que quer mostrar 0 termos.

--- Ex062-Super Progressão Aritmética.py
+++ Ex062-Super Progressão Aritmética.py
@@ -5,16 +5,40 @@ razão=int(input('Qual a razaão da PA: '))
 termo=primeiro
 cont=1
 total=0
-mais=10
+mais=5
+
 while mais !=0:
     total+=mais
     while cont<=total:
         print('{}->'.format(termo),end='')
         termo=termo+razão
         cont=cont+1
+
     print('PAUSA')
     mais=int(input('Quantos termo você quer a mais: '))
-print('FIM')
+
+print('Progressão finalizada com {} termos.'.format(total))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
