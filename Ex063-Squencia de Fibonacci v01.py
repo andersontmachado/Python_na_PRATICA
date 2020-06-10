aa5287018db0cf38b62d927b9c0362dc0f6a1fbd
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-06-10 20:24:39 -0300

    Exercício Python 063: Escreva um programa que leia um número N inteiro qualquer e mostre na tela os N primeiros elementos de uma Sequência de Fibonacci.

--- Ex063-Squencia de Fibonacci v01.py
+++ Ex063-Squencia de Fibonacci v01.py
@@ -1,8 +1,18 @@
 print('Sequencia de Fibonacci')
 print('-=' * 20)
-f = int(input('Digite quantos termos você quer mostar: '))
+termos = int(input('Digite quantos termos você quer mostar: '))
 t1 = 0
 t2 = 1
-print('{} -> {}'.format(t1,t2))
+print('{} -> {}'.format(t1,t2),end='')
+cont=3#contador começa com 3,pq os 2 primeiro termos ja foi feito, o 0 e 1
+while cont <=termos:  #n pq o usuario vai digitar o termo e agente não sabe.
+    t3=t2+t1
+    print('{} ->'.format(t3),end=')
+          t1=
+
+
+
+
+
 
 
