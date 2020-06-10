528052fabda8a618811d242895326aafd469e430
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-06-09 21:31:55 -0300

    Exercício Python 062: Melhore o DESAFIO 061, perguntando para o usuário se ele quer mostrar mais alguns termos. O programa encerrará quando ele disser que quer mostrar 0 termos.

--- Ex062-Super Progressão Aritmética.py
+++ Ex062-Super Progressão Aritmética.py
@@ -1,11 +1,20 @@
 print('Gerador de uma PA')
-print('=-'*10)
+print('=>'*10)
 primeiro=int(input('Primeiro termo: '))
-razão=int(input('Razão da P.A: '))
+razão=int(input('Qual a razaão da PA: '))
 termo=primeiro
 cont=1
-while cont <=20:
-    print('{}->'.format(termo),end='')#tiramos o final da linha..
-    termo=termo+razão
-    cont+=1
+total=0
+mais=10
+while mais !=0:
+    total+=mais
+    while cont<=total:
+        print('{}->'.format(termo),end='')
+        termo=termo+razão
+        cont=cont+1
+    print('PAUSA')
+    mais=int(input('Quantos termo você quer a mais: '))
 print('FIM')
+
+
+
