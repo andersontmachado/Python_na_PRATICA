0e0aafcb0cca4047053ea980e5d5a12eeaea6568
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-06-11 10:26:40 -0300

    Exercício Python 063: Escreva um programa que leia um número N inteiro qualquer e mostre na tela os N primeiros elementos de uma Sequência de Fibonacci.

--- Ex063-Squencia de Fibonacci v01.py
+++ Ex063-Squencia de Fibonacci v01.py
@@ -1,18 +1,29 @@
-print('Sequencia de Fibonacci')
-print('-=' * 20)
-n = int(input('Digite quantos termos você quer mostar: '))
-print('-='*20)
-t1 = 0 #sera fixo,começamos com zero
-t2 = 1#pra fazer Fibonacci essa questao utilizamos o 0 e 0 1 no começo,fixo
+print('-'*25)
+print('Frequencia de Fibonacci')
+print('-'*25)
+n=int(input('Quantos termos você quer mostrar: '))
+print('-='*25)
+t1=0
+t2=1
 print('{} -> {}'.format(t1,t2),end='')
-cont=3
+cont=3#fizemos essa variavel para ja que tem t1 vale 0,t2 vale 1,criamos o contador com 3 em diante.
 while cont <= n:
-    t3 = t1 + t2#o t3 eh o t1 + t2
+    t3=t1+t2#criamos o t3,pq o t1 vale 0, o t2 vale 1,pra poder fazer essa FREQUENCIA.ficam fixo
     print(' -> {}'.format(t3),end='')
-    t1=t2 #t1 passa a ser o t2
-    t2=t3 #o t2 passa a ser t3,vai andando,pra não fazer vários termos desse,ai entra o while
-    cont+=1
+#depois que foi feito essa do t3,eliminamos eles,ai o t1 passa a ser t2,t2 passa ser t3,e o t3 passar ser t1+t2,vai indo assim
+    t1=t2
+    t2=t3
+    cont+=1#ir contando
 print(' -> FIM')
+''' Lembrando que não é contagem de todos os numeros,e sim do t1,t2,o t3 vale o t1+t2,ai como se contasse a e b,o c retorna
+pra contar dinovo,exemplo...0->1->fixo,que valw t1=0,e t2=1,depois o t3 é o t1+t2,então na proxima sequencia,dá
+0+1=1,a pfroxima sequencia 0+1=1+1=2+3=5+3=8 e assim por diante
+                               -   -   -   -'''
+
+
+
+
+
 
 
 
