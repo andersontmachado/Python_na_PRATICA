d532ad29e55010d9371f273023ee1ed8e9164357
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-06-22 22:06:27 -0300

    Update Brincando.py

--- Brincando.py
+++ Brincando.py
@@ -28,35 +28,36 @@ while opção !=5:
     [5]Sair''')
     opção=int(input('Qual sua opção:'))
     nasc=2020-cat
-    if opção ==1 and idade>=18:
+while opção ==1:
+    if  idade>=18:
         print('Você tem {} anos,pode ir!'.format(nasc))
-        maior+=1
+        cont+=1
     elif opção ==1 and idade<18:
         print('Você tem {} anos,é um bebe ainda,não pode sair!'.format(nasc))
-        menor=menor+1
-    elif opção ==2 and idade <10:
+        cont+=1
+while opção ==2 and idade <10:
         print('A sua mãe vai te levar pra conhecer a palavra do senhor!')
         menor+=1
-    while opção ==3:
-        while cont==0:
-            if idade < 10:
-                print('Você nasceu em \033[31m{}\033[me tem \033[32m{}\033[manos'.format(cat,idade))
-                print('Categoria Fraldinha!')
-                cont+=1
-            elif idade > 10 and idade <20:
-                print('Você nasceu em \033[31m{}\033[m e tem \033[32m{}\033anos'.format(cat,idade))
-                print('Categoria Pré mirim!')
-                cont+=1
-            elif idade>20 and idade <35:
-                print('Você nasceu em \033[31m{}\033[m e tem \033[31m{}\033[m anos'.format(cat,idade))
-                print('Categoria Infantil')
-                cont+=1
-            elif idade >35 and idade <45:
-                print('Você nasceu em \033[31m{}\033[m e tem \033[m{} anos'.format(cat,idade))
-                print('Categoria Adulto')
-                cont+=1
+while opção ==3:
+    while cont==0:
+        if idade < 10:
+            print('Você nasceu em \033[31m{}\033[me tem \033[32m{}\033[manos'.format(cat,idade))
+            print('Categoria Fraldinha!')
+            cont+=1
+        elif idade > 10 and idade <20:
+            print('Você nasceu em \033[31m{}\033[m e tem \033[32m{}\033anos'.format(cat,idade))
+            print('Categoria Pré mirim!')
+            cont+=1
+        elif idade>20 and idade <35:
+            print('Você nasceu em \033[31m{}\033[m e tem \033[31m{}\033[m anos'.format(cat,idade))
+            print('Categoria Infantil')
+            cont+=1
+        elif idade >35 and idade <45:
+            print('Você nasceu em \033[31m{}\033[m e tem \033[m{} anos'.format(cat,idade))
+            print('Categoria Adulto')
+            cont+=1
 
-                print('FIM')
+            print('FIM')
 print('ACABOOOOOOOO')
 ##esse projeto estou desenvolvendo para que eu possa aprender o basico da programação.
 ##Esse projeto foi para o GIT HUB
