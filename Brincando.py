274d632c35da2bbef44fe7ea7a8ad4394e877bf3
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-06-25 20:40:58 -0300

    Fazendo tudo dinovo pra ver se aprendo na pratica.

--- Brincando.py
+++ Brincando.py
@@ -10,15 +10,15 @@ idade=atual-ano
 cat=ano
 cont=0
 while opção !=5:
-    while ssopção==0:
+    while soma==0:
         if idade <18:
             sleep(0)
             print('Você tem \033[32m{}\033[m anos,é de menor!'.format(idade))
-            soma+=1
+            soma=soma+1
         else:
             sleep(0)
             print('Você tem \033[31m{}\033[m anos,é de maior!'.format(idade))
-            soma+=1
+            soma=soma+1
     print('''    [1]Balada
     [2]Igreja
     [3]Futebol
@@ -27,12 +27,16 @@ while opção !=5:
     opção = int(input('Qual sua opção:'))
     nasc = 2020 - cat
     if opção ==1:
-        if idade<10:
-            print('Você tem {} anos e deve ir com sua mãe pra igreja!'.format(nasc))
+        if idade<18:
+            falta=18-nasc
+            print('Você tem {} anos e ainda não pode sair!'.format(nasc))
+            print('Falta ainda {} anos pra fazer 18.'.format(falta))
+            print('='*40)
             cont+=1
-        elif idade >=10:
-            print('Você tem {} anos e podr ir sozinho para igreja!'.format(nasc))
+        elif idade >=18:
+            print('Você tem {} anos e a responsabilidade passa a ser sua.'.format(nasc))
             cont+=1
+
 
 
 print('FIM')
