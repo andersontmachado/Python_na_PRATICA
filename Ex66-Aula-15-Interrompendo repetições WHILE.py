29498539a411acdf51123a90fc2feeb9519998e0
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-07-08 21:33:53 -0300

    Colocando com F STRING,uma nova fórmula do python,uma pep ...não precisa colocar .format..

--- Ex66-Aula-15-Interrompendo repetições WHILE.py
+++ Ex66-Aula-15-Interrompendo repetições WHILE.py
@@ -1,6 +1,12 @@
 n=0
 s=0
-while n!= 999:
+while True:
     n=int(input('Digite um número:'))
+    if n ==999:
+        break
     s+=n
-print('A soma de {} será {}'.format(n,s))
+'''print('A soma de {} será {}'.format(n,s))
+IREMOS aprender daqui adiante com F string,uma nova pep que o
+python faz,não precida por format mais,colocamos f depois aspas.'''
+print(f'A SOMA DO VALOR {n} É {s}')
+
