0cf04212207c13ba1b9fb3e6a26c6dfdbb008328
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-07-09 22:36:50 -0300

    Exercício Python 066: Crie um programa que leia números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre elas (desconsiderando o flag).

---
+++ Ex067-Tabuada v.03.py
@@ -0,0 +1,14 @@
+opção=''
+cont=0
+while True:
+    t=int(input('Quer ver a tabuada de qual valor: '))
+    print('=-' * 30)
+    for c in range(1,11):
+        print(f'{t} x {c:2} = {t*c}')
+    opção=str(input('Quer sair? [S/N]').strip().upper()[0])
+    if opção in 'Nn':
+        print('Quer ver a tabuada de qual valor: ')
+        cont += 1
+    if opção not in 'Ss':
+        break
+print('PROGRAMA ENCERRADO')
