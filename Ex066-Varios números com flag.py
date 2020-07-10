9e48cf5386b4ac5ad49a2e4609fd7fe2e1e18fdf
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-07-09 21:29:53 -0300

    Exercício Python 066: Crie um programa que leia números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre elas (desconsiderando o flag).

--- Ex066-Varios números com flag.py
+++ Ex066-Varios números com flag.py
@@ -1,10 +1,10 @@
 soma= cont=0
-while True:
+while True:#Verdadeiro,sempre vai ser verdade,chama loop infinito,o break é para interromper
         n=int(input('Digite um valor (999 para sair): '))
-        if n == 999:
+        if n == 999:#se eu digitar 999 ele vai sair,e vai me retorna a soma dos valores
             break
-        cont+=1
-        soma+=n
+        cont+=1 #vai contar os valores digitados.
+        soma+=n #vai somar os valores
 print(f'A Soma dos \033[31m{cont}\033[m valores foi de \033[32m{soma}\033[m')
 #coloquei cores nas respostas.....
 
