3547be9d2f600f92ee514650ed8150d6cb5e20a7
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-07-10 21:47:03 -0300

    Update Ex067-Tabuada v.03.py

--- Ex067-Tabuada v.03.py
+++ Ex067-Tabuada v.03.py
@@ -5,10 +5,9 @@ while True:
     print('=-' * 30)
     for c in range(1,11):
         print(f'{t} x {c:2} = {t*c}')
-    opção=str(input('Quer sair? [S/N]').strip().upper()[0])
-    if opção in 'Nn':
-        print('Quer ver a tabuada de qual valor: ')
-        cont += 1
-    if opção not in 'Ss':
-        break
+opção=str(input('Quer sair? [S/N]').strip().upper()[0])
+if opção in 'Nn':
+    print('Quer ver a tabuada de qual valor: ')
+elif opção in 'Ss':
+    break
 print('PROGRAMA ENCERRADO')
