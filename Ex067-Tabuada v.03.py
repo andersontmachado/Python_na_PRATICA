9a14e5fa7ece235c6a25e92e73345ec00cf73dc3
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-07-14 20:51:45 -0300

    Exercício Python 067: Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário. O programa será interrompido quando o número solicitado for negativo.

--- Ex067-Tabuada v.03.py
+++ Ex067-Tabuada v.03.py
@@ -1,4 +1,4 @@
-while True:
+'''while True:
     t=int(input('Quer ver a tabuada de qual valor [0 para SAIR]: '))
     print('=-' * 30)
     if t == 0:#implementei o zero para sair,pois é nulo.
@@ -6,11 +6,12 @@ while True:
     for c in range(1,11):
         print(f'{t} x {c:2} = {t*c}')
 print('PROGRAMA ENCERRADO')
-...
+...'''
 
-'''opção=''
+opção=' '
 while True:
-    t=int(input('Quer ver a tabuada de qual valor: '))
+    if opção not in 'NS':
+        t=int(input('Quer ver a tabuada de qual valor: ').strip().upper()[0])
     print('=-' * 30)
     opção=str(input('Quer sair: [S/N]'))
     if opção in 'Nn':
@@ -21,6 +22,8 @@ while True:
         print(f'{t} x {c:2} = {t*c}')
 
 print('PROGRAMA ENCERRADO!')
-programa fiz com resposta para não sair e sair,esta 99% certo,só que ele
-da uma resposta no final com o numero da tabuada,não conseguii tirar.'''
+
+
+#programa fiz com resposta para não sair e sair,esta 99% certo,só que ele
+#da uma resposta no final com o numero da tabuada,não conseguii tirar.'''
 
