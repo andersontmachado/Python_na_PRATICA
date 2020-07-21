ad844718108c2a68d11875f0542bab436112bf75
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-07-20 22:34:02 -0300

    Update Ex070-Estastica em Produtos.py

--- Ex070-Estastica em Produtos.py
+++ Ex070-Estastica em Produtos.py
@@ -1,26 +1,33 @@
-produto=preço=cont=0
-totalcompra=0
+cont=0
+total=0
 totmil=0
-menorvalor=0
+menor=0
+barato=''
 while True:
     produto=str(input('Nome do Produto: '))
     preço=float(input('Preço: R$'))
+    cont=cont+1#assim que ele ler o preço,vai começar a contar mais um.
+    total=+preço
+    if preço > 1000:
+        totmil+=1
+    if cont ==1:#isso é,se for o primeiro produto
+        menor=preço
+        barato=produto
+    else:
+        if preço<menor:
+            menor=preço
+            barato=produto
     opção=' '
-    totalcompra=totalcompra+preço
     while opção not in 'SN':
         opção=str(input('Quer continuar: [S/N]').strip().upper()[0])
     if opção == 'N':
         break
-    if opção == 'S':
-        cont+=1
-    if preço >= 1000:
-        totmil+=1
-    if preço <1000:
-        menorvalor
 
-print(f'O total da compra foi de {totalcompra:.2f}')
+
+
+print(f'O total da compra foi de {total:.2f}')
 print(f'Temos {totmil} com mais de R$1.000,00')
-print(f'O produto mais barato foi {produto} que custa {preço} ')
+print(f'O produto mais barato foi {produto} que custa R${preço:.2f} ')
 
 
 
