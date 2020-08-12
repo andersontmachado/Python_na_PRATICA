8665bc00c5e78839a4bf5cb3282d97de43ed9f53
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-08-11 21:37:34 -0300

    Exercício Python 071: Crie um programa que simule o funcionamento de um caixa eletrônico. No início, pergunte ao usuário qual será o valor a ser sacado (número inteiro) e o programa vai informar quantas cédulas de cada valor serão entregues.

--- Ex071-Simulador de CAIXA ELETRONICO.py
+++ Ex071-Simulador de CAIXA ELETRONICO.py
@@ -3,7 +3,7 @@ print('{:^30}'.format('BANCO ANDERSON'))
 print('='*30)
 saque=int(input('Qual valor você quer sacar?R$'))
 total=saque
-céd=50
+céd=100
 totalcéd=0
 while True:
     if total>=céd:
@@ -12,14 +12,16 @@ while True:
     else:
         if totalcéd>0:
             print(f'Total de {totalcéd} cédulas de R${céd}')
-        if céd==50:
+        if céd==100:
+            céd=50
+        elif céd==50:
             céd=20
-        elif céd==20:
+        elif céd ==20:
             céd=10
-        elif céd ==10:
-            céd=1
         totalcéd=0
         if total ==0:
             break
+print('='*50)
+print('Muito Obrigado por utilizar o BANCO ANDERSON')
 
 
