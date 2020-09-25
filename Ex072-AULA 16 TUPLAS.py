774483d492016db53fcf5e37dd158c6558c4c507
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-09-24 21:34:16 -0300

    Nessa aula, vamos aprender o que são TUPLAS e como utilizar tuplas em Python. As tuplas são variáveis compostas e imutáveis que permitem armazenar vários valores em uma mesma estrutura, acessíveis por chaves individuais.

---
+++ Ex072-AULA 16 TUPLAS.py
@@ -0,0 +1,11 @@
+times=('São Paulo','Corinthians','Palmeiras','Santos')
+#TUPLAS SÃO IMUTAVEIS,OU seja,não conseguimos mudar os itens
+#times[1]='Vasco'da esse erro>TypeError: 'tuple' object does not support item assignment
+#não consiguimos atribir valores a tupla.
+for futebol in times:
+    print(f'Os times grande paulistas são {futebol}')
+print('TIMES CAMPEÕES')
+
+
+
+
