317b49c9fe7e993dfcc01ed4cdfb175a7acc30c9
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-09-29 21:05:34 -0300

    Exercício Python 072: Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até vinte. Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.

--- Ex072-Número por Extenso.py
+++ Ex072-Número por Extenso.py
@@ -1,1 +1,3 @@
-cont='zero','um','dois','três','quatro'
+cont=('zero','um','dois','três','quatro','cinco','seis','sete',
+      'oito','nove','dez','onze','doze','treze','quatorze','quinze',
+      'dezesseis','dezessete','dezoito','dezenove','vinte')
