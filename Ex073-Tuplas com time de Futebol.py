97c0e059684a39a033244a8de5c310f0d878a95c
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-10-01 21:19:35 -0300

    Create Ex073-Tuplas com time de Futebol.py

---
+++ Ex073-Tuplas com time de Futebol.py
@@ -0,0 +1,20 @@
+tabela=('Atlético - MG','Internacional','São Paulo','Palmeiras','Vasco','Flamengo',
+'Fluminense','Sport','Santos','Fortaleza','Athetico - PR','Ceará','Atlético - GO',
+'Corinthians','Grêmio','Bahia','Coritiba','Bragantino','Botafogo','Goiás')
+print('Os cinco primeiro do Brasileirão são',tabela[1:6])
+print('Os últimos quatro colocados são',tabela[-4:])
+print('Times em ordem alfabética',sorted(tabela))
+print(f'O Santos está na {tabela.index("Santos")+1}ª posição.')
+'''o INDEX ele procura a posição que voce quer,daí coloca entre parenteses e acrescenta duas aspas
+o time que vc escolheu +1 um,pq do mais1,pq ele começa a contagem com zero,pro normal sera
+a posição 8,mais na verdade esta em nono.'''
+print(list(tabela))
+
+
+
+#Esse exercicío até a ordem alfabetica acertei sozinho,sem olhar,o Index não ia lembrar
+
+
+
+
+
