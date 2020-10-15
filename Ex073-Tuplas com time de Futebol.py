7dfd848005683d5f9e15764f09ec8526aaca8e01
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-10-15 20:57:40 -0300

    Exercício Python 73: Crie uma tupla preenchida com os 20 primeiros colocados da Tabela do Campeonato Brasileiro de Futebol, na ordem de colocação. Depois mostre:
    a) Os 5 primeiros times.
    
    b) Os últimos 4 colocados.
    
    c) Times em ordem alfabética.
    
    d) Em que posição está o time da Chapecoense.

--- Ex073-Tuplas com time de Futebol.py
+++ Ex073-Tuplas com time de Futebol.py
@@ -9,7 +9,7 @@ print(f'O Santos está na {tabela.index("Santos")+1}ª posição.')
 o time que vc escolheu +1 um,pq do mais1,pq ele começa a contagem com zero,pro normal sera
 a posição 8,mais na verdade esta em nono.'''
 print(list(tabela))
-##coloquei a lista aqui pra tentar inumerar do 1 ao 20,mais sem sucesso,um debaixo do outro,
+###coloquei a lista aqui pra tentar inumerar do 1 ao 20,mais sem sucesso,um debaixo do outro,
 
 
 ####Esse exercicío até a ordem alfabetica acertei sozinho,sem olhar,o Index não ia lembrar
