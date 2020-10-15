742acca4d939197286e78b90a0f1c7115dfe2eca
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-10-15 20:48:48 -0300

    Exercício Python 074: Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla. Depois disso, mostre a listagem de números gerados e também indique o menor e o maior valor que estão na tupla.

--- Ex074-Maior e Menor valores em TUPLA.py
+++ Ex074-Maior e Menor valores em TUPLA.py
@@ -1,4 +1,6 @@
 from random import randint
 numeros=(randint(1,10),randint(1,10),randint(1,10),
          randint(1,10),randint(1,10))
-print(numeros)
+print(f'Os numero sorteados foram:\n',end='')
+for n in numeros:
+    print(n)
