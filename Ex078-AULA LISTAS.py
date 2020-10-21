2a3bd66b848fc13396095ab5eb28df7c1cd07adc
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-10-20 21:03:22 -0300

    Conhecendo as LISTAS.

---
+++ Ex078-AULA LISTAS.py
@@ -0,0 +1,16 @@
+'''palavras=['anderson','giovanna','mulher','homem','carro','pandeiro','boné']
+palavras.sort(reverse=True)
+print(f'Essa lista tem {len(palavras)} elementos')'''
+
+
+
+num=[0,4,5,6,8,2,1]
+num.insert(2,100)
+print(num)
+#primeiro elemento entre parenteses é o indice,então serio 0,1,2,3,4,5,6,quando fico 2 e 10,
+# o 2 é onde vai o novo elemento que é o 10.
+
+
+
+
+
