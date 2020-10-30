7b149a5b8587a9345c61afd8de9498c6d349ae6f
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-10-29 22:05:59 -0300

    Cadastrando TORCEDOR SAOPAULINO

---
+++ Cadastro de Torcedor.py
@@ -0,0 +1,7 @@
+print('-='*25)
+print('UNICID-PROJETO INTEGRADO PARA VENDAS DE INGRESSOS')
+print('-='*25)
+torcedor=str(input('Digite seu nome e Sobrenome: '))
+idade=int(input('Quantos anos você tem? '))
+cadastro=int(input('Digite seu cpf: '))
+
