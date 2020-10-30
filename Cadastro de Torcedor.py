4654f940be3a19438d5cd4b42ee52793db81cd3b
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-10-29 22:27:40 -0300

    Cadastrando TORCEDOR SAOPAULINO

--- Cadastro de Torcedor.py
+++ Cadastro de Torcedor.py
@@ -1,9 +1,14 @@
+from time import sleep
 print('-='*25)
 print(f'{"UNICID-CADASTRO DO TORCEDOR":^40}')
 print('-='*25)
 print(f'{"PROJETO INTEGRADO EM ANÁLISE E DESENVOLVIMENTO DE SISTEMAS"}')
 print('*'*60)
-torcedor=str(input('Digite seu nome e Sobrenome: '))
-idade=int(input('Quantos anos você tem? '))
-cadastro=int(input('Digite seu cpf: '))
+sleep(2)
+while True:
+    torcedor=str(input('Digite seu Nome e Sobrenome: '))
+    print(f'Bem vindo {torcedor}')
+    idade=int(input('Quantos anos você tem? '))
+    cadastro=int(input('Digite seu cpf: '))
+
 
