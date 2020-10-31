8aef1a1ac65afac432e3a2078774a780229d3ae3
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-10-30 21:32:15 -0300

    atualizando

--- Cadastro de Torcedor.py
+++ Cadastro de Torcedor.py
@@ -11,23 +11,31 @@ while True:
     torcedor=str(input('Digite seu Nome e Sobrenome: ').strip().upper())
     print(f'Bem vindo {torcedor}')
     idade=int(input('Quantos anos você tem? '))
-    cpf=str(input('Digite seu cpf: '))
-    cpf=str
-    tamanho_cpf=len(cpf)
-    print(tamanho_cpf)
+    cpf=str(input('Digite seu cpf: ').strip())
+    if len(cpf)<11:
+        cpf=cpf.zfill(11)
+    tamanho_cpf =str(input('{}.{}.{}-{}'.format(cpf[:3], cpf[3:6], cpf[6:9], cpf[9:])))
+    print('Seu cpf é válido')
 
 
 
 
 
 
-   """while True:
+
+
+
+
+
+
+'''while True:
         ingresso=int(input('Deseja quantos ingressos: '))
         if ingresso > 0 and ingresso <=3:
             print('Válido')
             break
         else:
             print('Inválido! Tem direito você e mais duas pessoas!')
-print('Tente Novamente....')"""
+print('Tente Novamente....')'''
+
 
 
