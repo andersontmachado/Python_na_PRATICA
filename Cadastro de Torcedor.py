d809c87611db4f34a802be37c65bc7ebf6efa6b9
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-10-30 22:25:30 -0300

    Fazendo o cpf,deu trabalho.. Ainda não sei class

--- Cadastro de Torcedor.py
+++ Cadastro de Torcedor.py
@@ -7,6 +7,7 @@ print('*'*60)
 sleep(1)
 ingresso=c=0
 cpf=str
+opção=' '
 while True:
     torcedor=str(input('Digite seu Nome e Sobrenome: ').strip().upper())
     print(f'Bem vindo {torcedor}')
@@ -24,16 +25,17 @@ while True:
             print('Válido')
         else:
             print('Inválido! Tem direito você e mais duas pessoas!')
+    while True:
         print('''Os Ingressos disponiveis são: 
-                         [1]Arquibancada
-                         [2]Numeradas
-                         [3]Camarotes
-                         [4]Sair''')
+         [1]Arquibancada
+         [2]Numeradas
+         [3]Camarotes
+         [4]Sair''')
         opção=str(input('Qual sua opção: '))
-        if opção ==1:
+            if opção ==1:
             ingresso_arq=str('O Ingresso para arquibancada custará R$30,00 Aceita[S/N]')
             if opção == 'Ss':
-                quantos_ingresso=str(input('Quantidade?'))
+            quantos_ingresso=str(input('Quantidade?'))
             else:
                 break
 print('Tente Novamente....')
