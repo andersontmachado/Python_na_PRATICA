1335ef38a9f0e2f1cce3bd91794a800618d58a49
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-11-03 21:51:50 -0300

    Atualizando

--- Cadastro de Torcedor.py
+++ Cadastro de Torcedor.py
@@ -7,10 +7,12 @@ print('*'*60)
 sleep(1)
 ingresso=c=0
 cpf=str
+opção=0
+c=0
 while True:
     torcedor=str(input('Digite seu Nome: ').strip().upper())
     sobreno=str(input('Digite seu sobrenome: ').strip().upper())
-    print(f'Bem vindo {torcedor,sobreno}')
+    print('Bem vindo {} {}'.format(torcedor,sobreno))
     idade=int(input('Quantos anos você tem? '))
     cpf=str(input('Digite seu cpf: ').strip())
     if len(cpf)<11:
@@ -19,4 +21,20 @@ while True:
     if len(cpf)>11:
         print('CPF INVÁLIDO')
         break
-while opção !=4:
+    print('''Pra qual local você quer assistir o jogo
+          [1]ARQUIBANCADA
+          [2]NUMERADA
+          [3]CADEIRAS
+          [4]SAIR''')
+    opção=int(input('Qual sua opção: '))
+    while opção == 1:
+        if opção ==1:
+            print('Arquibancada você optou,custa R$30,00.')
+            opção+=1
+        elif opção==2:
+            print('As numeradas custará R$50,00')
+            opção+=1
+
+
+print('saiu..')
+
