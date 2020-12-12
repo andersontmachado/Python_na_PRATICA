399d4208239c923195e224d426f7eb487c1ede1c
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-12-11 21:20:32 -0300

    Acrescentando valores,no metodo.

--- Curso FIAP-PYTHON/MétodoAPPEND.py
+++ Curso FIAP-PYTHON/MétodoAPPEND.py
@@ -2,9 +2,11 @@ inventario=[]
 resposta="S"
 while resposta=="S":
     inventario.append(input("Equipamento: "))
-    inventario.append(float(input("Valor: ")))
+    inventario.append(float(input("Valor:R$ ")))
     inventario.append(int(input("Número Serial: ")))
     inventario.append(input("Departamento: "))
     resposta=input("Digite \"S\" para continuar: ").upper()
-#a palavra APPEND é para ACRESCENTAR,então nesse código apliquei esse método
-#onde agente consegue adiciona o produto,o valor,a serial,departamento.
+"""a palavra APPEND é para ACRESCENTAR,então nesse código apliquei esse método
+onde agente consegue adiciona o produto,o valor,a serial,departamento."""
+
+
