e1ce5ec73dae450d8cb8c39b18e1ff87d82cdf9f
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-12-11 21:04:46 -0300

    Curso FIAP

--- Escala COOP.py
+++
@@ -1,7 +0,0 @@
-print('=-'*20)
-print('{:^30}'.format('Escala COOP'))
-print('=-'*20)
-for c in range(1,23):
-    fiscal:str(input('{}ºfiscal: '.format(c)))
-
-
--- listas bricando.py
+++
@@ -1,6 +0,0 @@
-time=[]
-for c in range(0,10):
-    time=str(input('Na posição {}'.format(c)))
-    print(time)
-
-
