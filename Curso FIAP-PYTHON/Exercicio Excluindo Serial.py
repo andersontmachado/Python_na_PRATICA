4556de613e5a3addf83057890d51620915f73678
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-12-14 11:35:59 -0300

    Descreva todos os equipamentos, quando quiser excluir,só colocar não e digitar a serial do equipamento.

---
+++ Curso FIAP-PYTHON/Exercicio Excluindo Serial.py
@@ -0,0 +1,26 @@
+equipamentos = []
+valores = []
+seriais = []
+departamentos = []
+resposta = "S"
+while resposta == "S":
+    equipamentos.append(input("Equipamento: ").strip().upper())
+    valores.append(float(input("Valor: ")))
+    seriais.append(int(input("Número Serial: ")))
+    departamentos.append(input("Departamento: "))
+    resposta = input("Digite \"S\" para continuar: ").upper()
+serial=int(input("\nDigite o serial do equipamento que será excluido: "))
+for indice in range(0, len(departamentos)):
+    if seriais[indice]==serial:
+        del departamentos[indice]
+        del equipamentos[indice]
+        del seriais[indice]
+        del valores[indice]
+        break
+
+for indice in range(0,len(equipamentos)):
+    print("\nEquipamento..: ", (indice+1))
+    print("Nome.........: ", equipamentos[indice])
+    print("Valor........: ", valores[indice])
+    print("Serial.......: ", seriais[indice])
+    print("Departamento.: ", departamentos[indice])
