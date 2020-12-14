9324ed20436e72ec6a4ddcfe47bc38e6d3afdde1
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-12-14 11:35:02 -0300

    Dando desconta para determinado equipamento que o usuario pediu com 10%

---
+++ Curso FIAP-PYTHON/Exercício Descontando valor.py
@@ -0,0 +1,17 @@
+equipamentos = []
+valores = []
+seriais = []
+departamentos = []
+resposta = "S"
+while resposta == "S":
+    equipamentos.append(input("Equipamento: "))
+    valores.append(float(input("Valor: ")))
+    seriais.append(int(input("Número Serial: ")))
+    departamentos.append(input("Departamento: "))
+    resposta = input("Digite \"S\" para continuar: ").upper()
+desconto=input("\nDigite o nome do equipamento que será descontado: ")
+for indice in range(0,len(equipamentos)):
+    if desconto==equipamentos[indice]:
+        print("Valor antigo: ", valores[indice])
+        valores[indice] = valores[indice] * 0.9
+        print("Novo valor: ", valores[indice])
