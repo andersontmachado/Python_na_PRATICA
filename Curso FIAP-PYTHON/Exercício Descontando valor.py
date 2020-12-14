b8c20df0fdfe6a19bbaa296b7e225f141da5056c
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-12-14 11:45:05 -0300

    Desconto com FOR para 10% do valor do equipamento

--- Curso FIAP-PYTHON/Exercício Descontando valor.py
+++ Curso FIAP-PYTHON/Exercício Descontando valor.py
@@ -15,3 +15,4 @@ for indice in range(0,len(equipamentos)):
         print("Valor antigo: ", valores[indice])
         valores[indice] = valores[indice] * 0.9
         print("Novo valor: ", valores[indice])
+        
