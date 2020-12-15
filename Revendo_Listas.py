b9ec81a8beb6717841f25ba05bbdaf962e86e965
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-12-14 21:34:32 -0300

    Create Revendo_Listas.py

---
+++ Revendo_Listas.py
@@ -0,0 +1,35 @@
+inventario=[]
+resposta='S'
+escola=[]
+valores=[]
+curso=[]
+while resposta =='S':
+    escola.append(input('Digite o nome da Escola: '))
+    curso.append(input('Digite o nome do curso: '))
+    valores.append(float(input('Digite o valor:R$ ')))
+    resposta=input('Digite \'S\' para continuar: ').upper()
+
+
+busca=input('Digite o nome da escola para busca: ')
+for indice in range(0,len(escola)):
+    if busca == escola[indice]:
+        print('Nome.....',curso[indice])
+        print('Valor R$ ',valores[indice])
+
+for indice in range(0,len(escola)):
+    print('Nome da Escola: ',(indice+1))
+    print('Escola...',escola[indice])
+    print('Curso....',curso[indice])
+    print('Valor....',valores[indice])
+    print('=-'*40)
+    
+
+
+
+
+
+
+
+
+
+
