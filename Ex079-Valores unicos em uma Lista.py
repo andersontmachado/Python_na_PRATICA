e45fc01f10e9f54c82f4127bd2779b0a99c04e93
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2020-12-15 21:07:51 -0300

    Relembrando

--- Ex079-Valores unicos em uma Lista.py
+++ Ex079-Valores unicos em uma Lista.py
@@ -1,4 +1,4 @@
-show=[]
+'''show=[]
 for cont in range(0,3):
     show.append(input('Digite os shows que mais gosta: ').upper())
 for c,v in enumerate(show):
@@ -6,7 +6,9 @@ for c,v in enumerate(show):
     print('=-'*30)
     print(f'Os shows que mais gosta é \033[0;31m{v}\033[m')
     print('=-'*30)
-print('Cheguei ao final da lista')
+print('Cheguei ao final da lista')'''
+#Exercicio feito para relembrar LISTAS, então improvisei montando shows.
+
 
 
 
