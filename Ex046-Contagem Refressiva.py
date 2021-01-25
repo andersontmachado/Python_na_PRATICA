f9477538cd3647cfa3866241cd2ef33dca810746
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2021-01-25 09:59:54 -0300

    Mundo 3
    Mundo 3

--- Ex046-Contagem Refressiva.py
+++ Ex046-Contagem Refressiva.py
@@ -1,13 +1,8 @@
 from time import sleep
-for c in range (10,-1, -1):
+for c in range (10,-1,-1 ):
     print(c)
     sleep(1)
 print('BUM BUM BUM POW POW BUUUUUUUM')
-
-
-
-
-
 
 
 
--- Ex078-AULA LISTAS.py
+++ Ex078-AULA LISTAS.py
@@ -7,9 +7,9 @@ print(f'Essa lista tem {len(palavras)} elementos')'''
 
 
 num=[0,4,5,6,8,2,1]
-num.insert(2,100)
+num.insert(4,99)
 print(num)
-#primeiro elemento entre parenteses é o indice,então serio 0,1,2,3,4,5,6,quando fico 2 e 10,
+#primeiro elemento entre parenteses é o indice,então serio 0,1,2,3,4,5,6,quando fico 2 e 100,
 # o 2 é onde vai o novo elemento que é o 10.
 
 
