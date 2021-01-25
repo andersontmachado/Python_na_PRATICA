108508e4dd87c0c04ecedb44189a047f23724f48
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2021-01-25 11:41:45 -0300

    Exercício Python 080: Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista, já na posição correta de inserção (sem usar o sort()). No final, mostre a lista ordenada na tela.

--- Ex078-AULA LISTAS.py
+++ Ex078-AULA LISTAS.py
@@ -1,16 +1,34 @@
-'''palavras=['anderson','giovanna','mulher','homem','carro','pandeiro','boné']
-palavras.sort(reverse=True)
-print(f'Essa lista tem {len(palavras)} elementos')'''
+'''#METODO APPEND QUE SIGNIFICA ADICIONAR
+#AS LISTAS SÃO MUTÁVEIS,OU SEJA,PODEMOS ADIONAR ELEMNTOS COM METODO APPEND,NA TUPLA NÃO PODE
+#QUANDO DA ESSE COMANDO,IRÁ ADICONAR SEMPRE NO FINAL DA LISTA'''
+lanche=['pizza','pão','hamburguer','hot dog','sorvete','pamonha']
+'''lanche.append('salada')
+print(lanche)'''
 
-#FIZ varios,com SORT,SORT(REVERSE),APPEND, INSERT,TODOS FUNCIONO
+
+
+'''O METODO APPEND IRÁ INSERIR SEMPRE NA ULTIMA POSIÇÃO,AGORA PRA VOCÊ ADICIONAR UM LANCHE,NA POSIÇÃO
+#DESEJADAS IREMOS USAR O METODO INSERT.
+lanche.insert(1,'esfiha')
+# o numero 3 é a posição que você quer,0 é a pizza,1 é o pão,o 2 será o hamburguer e a esfiha sera 3,assim sucessivamente
+print(lanche)'''
+
+
+'''O METODO DEL IRÁ REMOVER ELEMENTOS
+SÃO TRÊS FORMAS DE REMOVER
+del lanche [3]# básico esse metodo,tenta eliminar na posição 3,que será hot dog.
+print(lanche)
+lanche.pop(3)#normalmente metodo pop elimina o ultimo elemento,mais você pode passar o parametro indicie que voce quer eliminar.
+print(lanche)
+lanche.remove('sorvete')#esse método ele elimina por centeudo,não por indice,vai idicar o valor.
+print(lanche)
+#o metodo REMOVE irá ser removido quando tiver na lista,então utilizamos 
+if 'pizza' in lanche:
+    lanche.remove('pizza') é uma maneira bem interessante e válido de utilizar esse metodo.
+#EM TODOS ESSES CASOS IRÁ ELIMINAR.,todos foram testados e funciono perfeitamente.'''
 
 
 
-num=[0,4,5,6,8,2,1]
-num.insert(4,99)
-print(num)
-#primeiro elemento entre parenteses é o indice,então serio 0,1,2,3,4,5,6,quando fico 2 e 100,
-# o 2 é onde vai o novo elemento que é o 10.
 
 
 
--- Ex080-Lista ordenada sem repetições.py
+++ Ex080-Lista ordenada sem repetições.py
@@ -1,1 +1,18 @@
-s
+lista=[] #conjunto vazio
+for c in range(0,5):
+    n=int(input(f'Digite um valor: '))
+    if c==0 or n>lista[-1]:#se ele é o primeiro ou maior que o ultimo
+        lista.append(n)
+        print('Adicionando ao final da lista...')
+    else:
+        pos=0
+        while pos <len(lista):#vai da posição zero até a última
+            if n < lista[pos]:
+                lista.insert(pos,n)
+                print(f'Adicionando na posição {pos} da lista')
+                break
+            pos+=1
+print(f'Os valores digitados em ordem foram \033[1;32m{lista}\033[m')
+
+
+
