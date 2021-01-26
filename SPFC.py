8351028b7d934b6743a164c6471ca51d2811f4b9
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2021-01-26 20:11:15 -0300

    Quantos anos tem seu time de coração.

--- SPFC.py
+++ SPFC.py
@@ -6,7 +6,7 @@ time=input('Qual seu time: ').upper()
 atual=date.today().year
 anos_time=int(input('Em que anos seu time foi fundado: '))
 resultado_anos=atual - anos_time
-print(f'Seu nome é {nome}, tem {idade} anos,seu time é o \033[1;32m{time}\033[m,hoje dia 25 de Janeiro completa \033[1;33m{resultado_anos}\033[m de idade')
+print(f'Seu nome é {nome}, tem {idade} anos,seu time é o \033[1;32m{time}\033[m,hoje dia 25 de Janeiro completa \033[1;33m{resultado_anos}\033[m anos de idade')
 sleep(2)
 print('\033[1;32mBORA COMEMORAR\033[m!!!!!')
 
