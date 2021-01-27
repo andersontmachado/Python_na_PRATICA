5c754b0052e34167c21f9a0d25c148373920eec2
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2021-01-26 21:04:18 -0300

    Aprendendo na teoria e colocando em pratica.

--- Ex078-AULA LISTAS.py
+++ Ex078-AULA LISTAS.py
@@ -1,20 +1,24 @@
 '''#METODO APPEND QUE SIGNIFICA ADICIONAR
 #AS LISTAS SÃO MUTÁVEIS,OU SEJA,PODEMOS ADIONAR ELEMNTOS COM METODO APPEND,NA TUPLA NÃO PODE
-#QUANDO DA ESSE COMANDO,IRÁ ADICONAR SEMPRE NO FINAL DA LISTA'''
+#QUANDO DA ESSE COMANDO,IRÁ ADICONAR SEMPRE NO FINAL DA LISTA
 lanche=['pizza','pão','hamburguer','hot dog','sorvete','pamonha']
-'''lanche.append('salada')
-print(lanche)'''
+lanche.append('salada')
+print(lanche)
+##########################################################################################################
+##########################################################################################################
 
 
 
-'''O METODO APPEND IRÁ INSERIR SEMPRE NA ULTIMA POSIÇÃO,AGORA PRA VOCÊ ADICIONAR UM LANCHE,NA POSIÇÃO
+O METODO APPEND IRÁ INSERIR SEMPRE NA ULTIMA POSIÇÃO,AGORA PRA VOCÊ ADICIONAR UM LANCHE,NA POSIÇÃO
 #DESEJADAS IREMOS USAR O METODO INSERT.
 lanche.insert(1,'esfiha')
 # o numero 3 é a posição que você quer,0 é a pizza,1 é o pão,o 2 será o hamburguer e a esfiha sera 3,assim sucessivamente
-print(lanche)'''
+print(lanche)
+##########################################################################################################
+##########################################################################################################
 
 
-'''O METODO DEL IRÁ REMOVER ELEMENTOS
+O METODO DEL IRÁ REMOVER ELEMENTOS
 SÃO TRÊS FORMAS DE REMOVER
 del lanche [3]# básico esse metodo,tenta eliminar na posição 3,que será hot dog.
 print(lanche)
@@ -25,10 +29,11 @@ print(lanche)
 #o metodo REMOVE irá ser removido quando tiver na lista,então utilizamos 
 if 'pizza' in lanche:
     lanche.remove('pizza') é uma maneira bem interessante e válido de utilizar esse metodo.
-#EM TODOS ESSES CASOS IRÁ ELIMINAR.,todos foram testados e funciono perfeitamente.'''
-#=-----===-------==---===--=-=======================----------------------
+#EM TODOS ESSES CASOS IRÁ ELIMINAR.,todos foram testados e funciono perfeitamente.'
+##########################################################################################################
+##########################################################################################################
 
-'''lanche[2]='cardapio'
+lanche[2]='cardapio'
 #se eu colocar o indice numero 6,ele da erro,portanto só vale o que esta nos indice,de 0 a 5 no exemplo do lanche
 lanche.insert(4,'cama')
 #pra inserir um item em alguma posição,devemos colocar Insert e nao append
@@ -42,8 +47,9 @@ if 'pizza' in lanche:#quando colocar string,deve-se sempre colocar entre aspas,s
 else:
     print('Não achei o desejado.')
 print(f'Essa lista tem {len(lanche)} elementos.')
-print(lanche)'''
-#===============================================------------------------------------------
+print(lanche)
+##########################################################################################################
+##########################################################################################################
 
 #COMEÇAR OUTRA MANEIRA,TODAS ESTAO FUNCIONANDO EM CIMA,PRA APRENDER MESMO LEGAL...
 valores=list()
@@ -51,14 +57,18 @@ for cont in range(0,5):
     valores.append(int(input('Digite um valor: ')))
     valores.sort()#fiz sozinho,deu certo,na hora de responder nas posiçoes,deu como ordem numerica...muito bom Anderson
 
-'''valores=list()#tanto valores=[](conjunto vazio,como list() da na mesma
+valores=list()#tanto valores=[](conjunto vazio,como list() da na mesma
 valores.append('zona')
 valores.append('puteiro')
-valores.append('cerveja')'''#esta como comentario,pq fiz direto do for in range,para o for c,v in enumerate.só isso...
+valores.append('cerveja')#esta como comentario,pq fiz direto do for in range,para o for c,v in enumerate.só isso...
 
 for c, v in enumerate(valores):#enumerate serve para mostrar tanto o indice(chaves),ele pega tanto a chave como valor.
     print(f'Na posição {c},encontrei o valor {v}...')
 print('Cheguei ao final da lista!!!')
+##########################################################################################################
+##########################################################################################################'''
+
+
 
 
 
