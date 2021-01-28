d5762357fe3fb45f9da581bff47f2b625253cc4c
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2021-01-27 21:56:16 -0300

    Exercício Python 081: Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, mostre:                                                                                                                                    A) Quantos números foram digitados.                                                                                                                    B) A lista de valores, ordenada de forma decrescente. C) Se o valor 5 foi digitado e está ou não na lista.

--- Ex081-Extraindo dados de uma lista.py
+++ Ex081-Extraindo dados de uma lista.py
@@ -4,11 +4,19 @@ while True:
     resp = str(input('Quer continuar [S/N]'))
     if resp in 'Nn':
         break
+
 print('=='*30)
 print(f'Você digitou {len(valores)} elementos.')
 valores.sort(reverse=True)
 print(f'Valor no formato descrecente {valores}')
+if 5 in valores:#coloquei ao contrario estava dando erro...ficar esperto...o 5 esta em valores:
+    print(f'O numero 5 esta na lista...')
+else:
+    print(f'O valor 5 não está na lista...')
 
+'''Esse exercicio foi muito simples,porem tive dificuldade,a base acertei,os nome,sort reverse eu sabia
+valores.append eu sabia,nao precisa colocar quando a resposta for sim uma variavel,tipo...resp='S',nao precisa
+'''
 
 
 
