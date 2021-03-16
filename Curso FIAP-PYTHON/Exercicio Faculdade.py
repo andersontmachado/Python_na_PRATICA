2c68c67cb1a9b84fd8a5a26f74e101674a7a6c8c
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2021-03-15 22:20:30 -0300

    altura do aluno

--- Curso FIAP-PYTHON/Exercicio Faculdade.py
+++ Curso FIAP-PYTHON/Exercicio Faculdade.py
@@ -5,9 +5,10 @@ for c in range(1,4):
     alunos = str(input(f'Digite o {c}º do aluno: ').upper())
     altura=float(input('Digite a altura do aluno: '))
     if altura > maior:
-        maior=altura
+        nome_aluno_maior=alunos
+        maior+=1
 
-print(f'Altura maior é {maior} e o aluno é {alunos}')
+print(f'Altura maior é {altura} e o aluno é {alunos}')
 
 
 
