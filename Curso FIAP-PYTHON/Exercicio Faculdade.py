c1326bb006f1b31ebe3cd37e60304f09c751d7e3
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2021-03-15 22:26:13 -0300

    Exercicio feito na faculdade,pois pediu pra fazer em JAVA,eu transformei em PYTHON.

--- Curso FIAP-PYTHON/Exercicio Faculdade.py
+++ Curso FIAP-PYTHON/Exercicio Faculdade.py
@@ -1,14 +1,11 @@
-alunos=0
 maior=0
-nome_aluno_maior=''
 for c in range(1,4):
-    alunos = str(input(f'Digite o {c}º do aluno: ').upper())
     altura=float(input('Digite a altura do aluno: '))
     if altura > maior:
-        nome_aluno_maior=alunos
-        maior+=1
+        maior=altura
 
-print(f'Altura maior é {altura} e o aluno é {alunos}')
+
+print(f'Altura maior é {maior} ')
 
 
 
