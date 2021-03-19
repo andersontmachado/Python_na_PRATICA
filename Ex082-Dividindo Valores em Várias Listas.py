43f936be936597bb8f84269c12e038855b74965c
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2021-03-18 22:44:53 -0300

    Exercício Python 082: Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, crie duas listas extras que vão conter apenas os valores pares e os valores ímpares digitados, respectivamente. Ao final, mostre o conteúdo das três listas geradas.

--- Ex082-Dividindo Valores em Várias Listas.py
+++ Ex082-Dividindo Valores em Várias Listas.py
@@ -1,20 +1,18 @@
 num = list()
-pares=list()
-impares=list()
+pares = list()
+impares = list()
 while True:
     num.append(int(input('Digite um numero: ')))
     resp = (input('Quer continuar: [S/N] '))
     if resp in 'Nn':
         break
-for i,v in enumerate(num):
-    if v % 2 ==0:
+for i, v in enumerate(num):
+    if v % 2 == 0:
         pares.append(v)
-    if v %2 == 1:
+    if v % 2 == 1:
         impares.append(v)
 print(f'Os valores digitados foram {num}')
 print(f'Os valores de pares são {pares}')
 print(f'Os valores digitados de impares são {impares}')
 
 '''Exercicio bom e facil,só relembrar algumas funções ja consigo fazer varios desses.'''
-
-
