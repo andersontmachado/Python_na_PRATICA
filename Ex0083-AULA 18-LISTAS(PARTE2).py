a76c22c7f027184a869ca16a31046697e7e13abf
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2021-03-29 22:14:37 -0300

    AULA 18 LISTAS DENTRO DE LISTAS

--- Ex0083-AULA 18-LISTAS(PARTE2).py
+++ Ex0083-AULA 18-LISTAS(PARTE2).py
@@ -10,4 +10,10 @@ print(galera)
 '''
 
 #######################################################################################
-galera=
+'''galera=[['João',19],['Ana',33],['Joaquim',13],['Maria',45]]
+print(galera)
+print(galera[0])
+print(galera[0][0])
+print(galera[2][1])
+print(galera[2][1])
+'''
