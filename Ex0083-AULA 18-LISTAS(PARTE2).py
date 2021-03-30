64ab9a18371f997a629f8cd1c15ca70bf3cbdc00
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2021-03-29 22:09:06 -0300

    Create Ex0083-AULA 18-LISTAS(PARTE2).py

---
+++ Ex0083-AULA 18-LISTAS(PARTE2).py
@@ -0,0 +1,13 @@
+'''pessoas=list()
+pessoas.append('Gustavo')
+pessoas.append(33)
+galera=list()
+galera.append(pessoas[:])
+pessoas[0]='Anderson'
+pessoas[1]='45'
+galera.append(pessoas[:])
+print(galera)
+'''
+
+#######################################################################################
+galera=
