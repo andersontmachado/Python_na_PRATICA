3ce91359775df4d9b49f144c48a96b63bccc9483
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2021-03-30 21:31:39 -0300

    LISTAS DENTRO DE LISTRAS

--- Ex0083-AULA 18-LISTAS(PARTE2).py
+++ Ex0083-AULA 18-LISTAS(PARTE2).py
@@ -17,3 +17,7 @@ print(galera[0][0])
 print(galera[2][1])
 print(galera[2][1])
 '''
+galera=[['João',19],['Ana',33],['Joaquim',13],['Maria',45]]
+for p in galera:
+    print(f'{p[0]} tem {p[1]} de idade')
+
