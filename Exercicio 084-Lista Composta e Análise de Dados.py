27504b66373b72cf899f5fc83d085b63c45324fb
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2021-04-01 22:15:19 -0300

    Exercício Python 084: Faça um programa que leia nome e peso de várias pessoas,        guardando tudo em uma lista. No final, mostre:                                         A) Quantas pessoas foram cadastradas.  B) Uma listagem com as pessoas mais pesadas.                                           C) Uma listagem com as pessoas mais leves.

--- Exercicio 084-Lista Composta e Análise de Dados.py
+++ Exercicio 084-Lista Composta e Análise de Dados.py
@@ -1,25 +1,30 @@
-pessoas=list()
-princ=list()
-mai=men=0
+pessoas = list()
+princ = list()
+mai = men = 0
 while True:
     pessoas.append(str(input('Nome: ')).upper())
     pessoas.append(float(input('Peso: ')))
-    if len(princ)==0:
-        mai=men=pessoas[1]
+    if len(princ) == 0:
+        mai = men = pessoas[1]
     else:
-        if pessoas[1]>mai:
-            mai=pessoas[1]
-        if pessoas[1]<men:
-            men=pessoas[1]
+        if pessoas[1] > mai:
+            mai = pessoas[1]
+        if pessoas[1] < men:
+            men = pessoas[1]
     princ.append(pessoas[:])
     pessoas.clear()
-    resp=str(input('Quer continuar: [S/N] '))
+    resp = str(input('Quer continuar: [S/N] '))
     if resp in 'Nn':
         break
-#print(f'Os dados foram {princ}')Era só pr testar,não estava pedindo essa analise
+# print(f'Os dados foram {princ}')Era só pr testar,não estava pedindo essa analise
 print(f'Ao todo você cadastrou {len(princ)} pessoas.')
-print(f'O maior peso foi {mai}kg')
-print(f'O menor peso foi {men}kg')
-
-
-
+print(f'O maior peso foi {mai}kg', end=' ')
+for p in princ:
+    if p[1] == mai:
+        print(f'O [{p[0]}]', end=' ')
+print()
+print(f'O menor peso foi {men}kg', end=' ')
+for p in princ:
+    if p[1] == men:
+        print(f'O [{p[0]}]', end=' ')
+print()
