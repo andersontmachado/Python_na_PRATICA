1a00ee285232e490ffecd683c540921405d3499c
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2021-04-05 22:28:46 -0300

    Exercício Python 085: Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que mantenha separados os valores pares e ímpares. No final, mostre os valores pares e ímpares em ordem crescente.

--- Ex085-Listas com Pares e Impares.py
+++ Ex085-Listas com Pares e Impares.py
@@ -1,5 +1,10 @@
 num=[[],[]]
 valor=0
-for c in range(0,8):#Ultimo elemento ele não conta,só quero 7 elementos!!
+for c in range(1,8):#Ultimo elemento ele não conta,só quero 7 elementos!!
     valor=int(input(f'Digite o {c}º valor: '))
-    if valor%2==0:#quando o restante da divisão for 0,é par.
+    if valor%2==0:#ou seja,quando o resto da divisão for zero,é par
+        num[0].append(valor)
+    else:
+        num[1].append(valor)
+print(f'Os valores pares foram {num[0]}.')
+print(f'Os valores impares foram {num[1]}.')
