7f4ae45ab5538de250cc1799de1ec612924153f5
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2021-04-09 22:46:40 -0300

    Aluno e Altura

--- Curso FIAP-PYTHON/Exercicio Faculdade.py
+++ Curso FIAP-PYTHON/Exercicio Faculdade.py
@@ -1,11 +1,23 @@
-maior=0
+pessoa=list()
+princ=list()
+mai=men=0
 for c in range(1,4):
-    altura=float(input('Digite a altura do aluno: '))
-    if altura > maior:
-        maior=altura
+    pessoa.append(str(input('Aluno: ')))
+    pessoa.append(float(input('Altura: ')))
+    if len(princ)==0:
+        mai=men==pessoa[1]
+    else:
+        if pessoa[1]>mai:
+            mai=pessoa[1]
+
+    princ.append(pessoa[:])
+    pessoa.clear()
+   # print(f'Os dados foram {princ}')Era só pr testar,não estava pedindo essa analise
+print(f'Ao todo você cadastrou {len(princ)} pessoas.')
+print(f'A maior altura foi {mai}m', end=' ')
 
 
-print(f'Altura maior é {maior} ')
+
 
 #####################
 #Existe só um jeito de fazer isso, eu queria colocar o nome do aluno
