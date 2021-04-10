5f1d58f6d1ae11ac36b835d2e635ddd81e4fd8e4
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2021-04-09 23:09:13 -0300

    Update Exercicio Faculdade.py

--- Curso FIAP-PYTHON/Exercicio Faculdade.py
+++ Curso FIAP-PYTHON/Exercicio Faculdade.py
@@ -1,7 +1,7 @@
 pessoa=list()
 princ=list()
 mai=men=0
-for c in range(1,4):
+for p in range(1,4):
     pessoa.append(str(input('Aluno: ')))
     pessoa.append(float(input('Altura: ')))
     if len(princ)==0:
@@ -9,11 +9,14 @@ for c in range(1,4):
     else:
         if pessoa[1]>mai:
             mai=pessoa[1]
+    princ.append(pessoa[:])
+    pessoa.clear()
 
 
    # print(f'Os dados foram {princ}')Era só pr testar,não estava pedindo essa analise
 print(f'Ao todo você cadastrou {len(princ)} pessoas.')
-print(f'[{c[0]}] tem {mai} metros.É o maior!')
+print(f'{pessoa[0]} ',end='')
+print(f' {mai} metros.É o maior!')
 
 
 
