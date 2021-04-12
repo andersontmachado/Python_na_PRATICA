c348e4454d2d875ecd917170ff4d426ce93e9ef3
Anderson Turibio Machado <anderson.ti.sup@gmail.com>
2021-04-12 19:15:09 -0300

    Aluno Altura
    Dificil,consegui graças a novo for.ai consegui sair a altura e o nome da pesssoa.

--- Curso FIAP-PYTHON/Exercicio Faculdade.py
+++ Curso FIAP-PYTHON/Exercicio Faculdade.py
@@ -1,30 +1,24 @@
-pessoa=[]
-princ=[]
-mai=men=0
-for p in range(1,4):
+pessoa = []
+princ = []
+mai = men = 0
+for p in range(1, 4):
     pessoa.append(str(input('Aluno: ')))
     pessoa.append(float(input('Altura: ')))
-    if len(princ)==0:
-        mai=pessoa[1]
+    if len(princ) == 0:
+        mai = pessoa[1]
     else:
-        if pessoa[1]>mai:
-            mai=pessoa[1]
+        if pessoa[1] > mai:
+            mai = pessoa[1]
     princ.append(pessoa[:])
     pessoa.clear()
-
-
-   # print(f'Os dados foram {princ}')Era só pr testar,não estava pedindo essa analise
 print(f'Ao todo você cadastrou {len(princ)} pessoas.')
-print(f'{pessoa} ',end='')
-print(f' {mai} metros.É o maior!')
-
-
+print(f'O maior peso foi {mai}kg.',end='')
+for p in princ:
+    if p[1] == mai:
+        print(f'[{p[0]}]')
 
 
 #####################
-#Existe só um jeito de fazer isso, eu queria colocar o nome do aluno
-#porem nao dá,tentei de tudo,nos foruns,consegue por só com tuplas ou listas
-#ainda nao fiz
-
-
-
+# Existe só um jeito de fazer isso, eu queria colocar o nome do aluno
+# porem nao dá,tentei de tudo,nos foruns,consegue por só com tuplas ou listas
+# ainda nao fiz
